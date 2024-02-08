f1b5d41245a7bffb955e8f9a7a1a4eca6163c036
heeneth <heeneth123@gmail.com>
2024-02-08 12:45:35 +0530

    ability to enter valid last name

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
-    public static void main(String[] args) {
+class UserName {
+    public void firstName() {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter your first name: ");
@@ -22,5 +23,40 @@ public class Main {
                     .println("Invalid first name " + firstName
                             + " Please start with 'cao' and have a minimum length of 3 characters. ");
         }
+
+    }
+
+    public void LastName() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your Last name: ");
+        String firstName = scanner.nextLine();
+
+        // Define the regular expression for the first name
+        String regex = "^[A-Z][a-z]{2,}$";
+        // {2,}quantifer it sayes small letter atleast two or more
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+
+        if (matcher.matches()) {
+            System.out.println("Valid Last name! " + firstName);
+        } else {
+            System.out
+                    .println("Invalid Last name " + firstName
+                            + " Please start with 'cao' and have a minimum length of 3 characters. ");
+        }
+
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserName user = new UserName();
+
+        user.firstName();
+        user.LastName();
+
     }
 }
