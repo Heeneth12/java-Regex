f2c7bb6f91441de261e3e7e772811cf13aa8b636
heeneth <heeneth123@gmail.com>
2024-02-08 12:24:29 +0530

    ablity to enter a valid first name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        // Define the regular expression for the first name
+        String regex = "^[A-Z][a-z]{2,}$";
+        // {2,}quantifer it sayes small letter atleast two or more
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+
+        if (matcher.matches()) {
+            System.out.println("Valid first name! " + firstName);
+        } else {
+            System.out
+                    .println("Invalid first name " + firstName
+                            + " Please start with 'cao' and have a minimum length of 3 characters. ");
+        }
     }
 }
